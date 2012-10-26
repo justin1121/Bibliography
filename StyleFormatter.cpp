e9dc167c37c81ccbdda8d40c82d73a5fd2931efa
Justin Patriquin <justinpatriquin@gmail.com>
2012-10-26 14:04:33 -0300

    Adding function bodies

---
+++ StyleFormatter.cpp
@@ -0,0 +1,27 @@
+#include "StyleFormatter.h"
+
+StyleFormatter::StyleFormatter(){
+}
+
+StyleFormatter::~StyleFormatter(){
+}
+
+OutputCitation StyleFormatter::format(){
+	return;
+}
+
+OutputCitation StyleFormatter::formatBook(){
+	return;
+}
+
+OutputCitation StyleFormatter::formatConference(){
+	return;
+}
+
+OutputCitation StyleFormatter::formatJournal(){
+	return;
+}
+
+OutputCitation StyleFormatter::formatTechnicalReport(){
+	return;
+}
